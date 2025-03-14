5bdb5ff8d07aa1fe2914b2596ad2d6fe94dc4928
silpac-12 <seancaplis@gmail.com>
2025-03-14 19:08:08 +0000

    changes to app.js

--- CS4227-Project/payment-module/frontend/src/App.js
+++ CS4227-Project/payment-module/frontend/src/App.js
@@ -1,19 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CartProvider } from "./store/cartContext";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
 
-const App = () => {
+function App() {
     return (
-        <CartProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/checkout" element={<Checkout />} />
-                </Routes>
-            </Router>
-        </CartProvider>
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/checkout" element={<Checkout />} />
+            </Routes>
+        </HashRouter>
     );
-};
+}
 
 export default App;
