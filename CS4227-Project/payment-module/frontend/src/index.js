3bd4a94a0ad9b10cb7b2c549c4d3c53710cb40bc
silpac-12 <seancaplis@gmail.com>
2025-03-14 19:22:04 +0000

    commit

--- CS4227-Project/payment-module/frontend/src/index.js
+++ CS4227-Project/payment-module/frontend/src/index.js
@@ -2,7 +2,7 @@ import { HashRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+//dd
 ReactDOM.render(
     <HashRouter>
         <App />
